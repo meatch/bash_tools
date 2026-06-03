bbe3c7fba9bd6f915ffa61d574eced45378cfda4
Mitch Gohman <mitch@thenounproject.com>
2026-06-03 14:53:39 -0600

    git: remove unused functions and aliases

--- src/app-support/git.sh
+++ src/app-support/git.sh
@@ -2,15 +2,6 @@
 # Git
 # --------------------------------------------------------------
 # Functions
-function grhard {
-    echo '>>>> Start clean, reset, and checkout';
-    set -x;
-    git clean -df;
-    git reset --hard;
-    git checkout .;
-    echo '<<<< End clean, reset, and checkout';
-}
-
 function grib() {
     git rebase -i origin/$1
 }
@@ -243,7 +234,5 @@ clean-worktrees() {
 alias gst="git status"
 alias gbv="git branch -vva"
 alias gbvg="git branch -vva | grep -i $1"
-alias gbvb="git for-each-ref --format='%(color:cyan)%(authordate:format:%m/%d/%Y %I:%M %p) %(align:25,left)%(color:yellow)%(authorname)%(end) %(color:reset)%(refname:strip=3)' --sort=authorname refs/remotes"
 alias glo="git log --oneline"
-alias glow="git log --oneline --all --graph --decorate"
 alias gfp="git fetch -p"
